2093a25136004022333b14592911c4609235ed9e
Artie Fuffkin <atch.cpp@gmail.com>
2016-11-28 19:37:45 +0100

    Git_Attr

---
+++ libgit2_cpp/Git_Attr.cpp
@@ -0,0 +1,25 @@
+#include "Git_Attr.hpp"
+
+void Git_Attr::add_macro(const std::string_view & name, const std::string_view &values)const
+{
+	check_for_error(git_attr_add_macro(c_parent_guts(), name.data(), values.data()));
+}
+
+void Git_Attr::cache_flush() const
+{
+	git_attr_cache_flush(c_parent_guts());
+}
+
+string_t Git_Attr::get(uint32_t flags, const path_name_t & path, const string_t & name) const
+{
+	const char* c_value_out;
+	check_for_error(git_attr_get(&c_value_out, c_parent_guts(), flags, path.c_str(), name.c_str()));
+
+	return c_value_out;
+}
+
+git_attr_t Git_Attr::value(const string_t & attr) const
+{
+	return git_attr_value(attr.c_str());
+#pragma message("Warning native libgit2 type returned")
+}
---
+++ libgit2_cpp/Git_Attr.hpp
@@ -0,0 +1,15 @@
+#pragma once
+#include "precompiled.hpp"
+class Git_Attr : Parent_Provider
+{
+public:
+	void add_macro(const std::string_view&, const std::string_view&)const;
+	void cache_flush()const;
+	string_t get(uint32_t, const path_name_t&,const string_t&)const;
+	git_attr_t value(const string_t&)const;
+#ifdef _FULL_IMPLEMENTATION_
+	git_attr_foreach
+	git_attr_get_many
+#endif
+};
+
